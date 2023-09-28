a47b703c833d2b0251d7affa592465c86f5d1b95
Godwin <115321545+Goqwin@users.noreply.github.com>
2023-09-27 21:00:05 -0600

    Just random designs to week2

--- app/week2/page.js
+++ app/week2/page.js
@@ -2,14 +2,14 @@ import Link from 'next/link';
 
 export default function ShoppingList() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 relative">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <Link href="/week3">
-  <h1 className="text-4xl">My Shopping List</h1>
-  <span className="absolute opacity-70 group-hover:opacity-100 text-gray-400 transition-opacity duration-300 ml-2">
-    Click Me ( ͡° ͜ʖ ͡°)
-  </span>
-</Link>
+        <Link href="/week3">
+          <h1 className="text-4xl">My Shopping List</h1>
+          <span className="absolute opacity-70 group-hover:opacity-100 text-gray-400 transition-opacity duration-300 ml-2">
+            Click Me ( ͡° ͜ʖ ͡°)
+          </span>
+        </Link>
         <div>
           <p>Name: Godwin JB Mercado</p>
           <p>Course section: CPRG 306B</p>
@@ -21,18 +21,23 @@ export default function ShoppingList() {
         </div>
       </div>
       <div>
-        <p> There's NOTHING?!</p>
-        <p> Wait a second... </p>
+        <p className='relative z-20'>There's NOTHING?!</p>
+        <p className='relative z-20'>Wait a second... </p>
         <p className="relative">
-  <Link href="/week3">
-    <span className="text-black hover:text-white hover:text-4xl transition-all duration-300">
-      SURPRISE
-    </span>
-  </Link>
-  <span className="absolute top-0 left-0 w-0 h-0 bg-black hover:bg-white transform scale-0 hover:scale-100 origin-top-left transition-all duration-300"></span>
-</p>
+          <Link href="/week3">
+            <span className="text-black hover:text-white hover:text-4xl transition-all duration-300">
+              SURPRISE
+            </span>
+          </Link>
+          <span className="absolute top-0 left-0 w-0 h-0 bg-black hover:bg-white transform scale-0 hover:scale-100 origin-top-left transition-all duration-300"></span>
+        </p>
       </div>
       <Link href="/">Home</Link>
+
+      
+      <h1 className="text-6xl text-yellow-600 absolute top-2/4 left-1/2 transform -translate-x-1/2 -translate-y-2/4 z-10 opacity-40">
+        ERROR
+      </h1>
     </main>
   );
 }
--- app/week4/page.js
+++ app/week4/page.js
@@ -1,35 +1,32 @@
-
 import Link from 'next/link';
 import NewItem from './new-item';
 
 const Page = () => {
-  const containerStyle = {
-    backgroundImage: 'url("https://i.pinimg.com/originals/05/51/7e/05517ebd2de835de4f408e686a2206c8.gif")',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-  };
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24' style={containerStyle}>
-      <h1 className="text-4xl text-orange-500 font-semibold text-center mb-8">
-  Godwin's Item Generator
-</h1>
-    <div className=" min-h-screen flex items-center justify-center">
-      <div className="bg-white rounded-lg shadow-md overflow-hidden w-[calc(100% - 40px)]">
-        <div className="flex justify-end items-center bg-[#333] text-white p-2">
-          <div className="text-lg cursor-pointer">🔴</div>
-          <div className="text-lg cursor-pointer ml-2">🟡</div>
-          <div className="text-lg cursor-pointer ml-2">🟢</div>
+    <div className="bg-cover bg-no-repeat" style={{ backgroundImage: 'url("https://i.pinimg.com/originals/05/51/7e/05517ebd2de835de4f408e686a2206c8.gif")' }}>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className="text-4xl text-orange-500 font-semibold text-center mb-8">
+          Godwin's Item Generator
+        </h1>
+        <div className="min-h-screen flex items-center justify-center">
+          <div className="bg-white rounded-lg shadow-md overflow-hidden w-[calc(100% - 40px)]">
+            <div className="flex justify-end items-center bg-[#333] text-white p-2">
+              <div className="text-lg cursor-pointer">🔴</div>
+              <div className="text-lg cursor-pointer ml-2">🟡</div>
+              <div className="text-lg cursor-pointer ml-2">🟢</div>
+            </div>
+            <div className="p-6">
+              <h2 className="text-2xl mb-4 text-black">Week 4: Assignment</h2>
+              <NewItem />
+              <Link href="/" className="mt-4 text-blue-500 hover:underline">
+                Back to Home
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className="p-6">
-          <h2 className="text-2xl mb-4 text-black">Week 4: Assignment</h2>
-          <NewItem />
-          <Link href="/" className="mt-4 text-blue-500 hover:underline">
-            Back to Home
-          </Link>
-        </div>
-      </div>
+      </main>
     </div>
-    </main>
   );
 };
+
 export default Page;
